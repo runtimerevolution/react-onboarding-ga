85a060e14c260e6c6649ecc74215fb7ee88f6100
Goncalo Adolfo <g.adolfo@runtime-revolution.com>
2023-04-14 15:27:02 +0100

    Add root component

--- react-router-tutorial/src/main.jsx
+++ react-router-tutorial/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Root from "./routes/root"
+import "./index.css"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const router = createBrowserRouter([{ path: "/", element: <Root /> }])
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 )
