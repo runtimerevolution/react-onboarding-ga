020a4f0750c1f4c87f76bd0b9ab21ee8a1ecca25
Goncalo Adolfo <g.adolfo@runtime-revolution.com>
2023-04-17 17:08:19 +0100

    Add contacts index component

--- react-router-tutorial/src/main.jsx
+++ react-router-tutorial/src/main.jsx
@@ -6,6 +6,7 @@ import Contact, { loader as contactLoader } from "./routes/contact"
 import { action as destroyAction } from "./routes/destroy"
 import EditContact, { action as editAction } from "./routes/edit"
 import ErrorPage from "./error-page"
+import Index from "./routes/index"
 import Root, { loader as rootLoader, action as rootAction } from "./routes/root"
 import "./index.css"
 
@@ -17,6 +18,7 @@ const router = createBrowserRouter([
         loader: rootLoader,
         action: rootAction,
         children: [
+            { index: true, element: <Index /> },
             {
                 path: "/contacts/:contactId",
                 element: <Contact />,
