8c4775a51a11527d310c9965bc8e1c15007867e6
Goncalo Adolfo <g.adolfo@runtime-revolution.com>
2023-04-26 15:47:47 +0100

    Add views alias

--- movies-app/vite.config.js
+++ movies-app/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
       '@components': path.resolve(__dirname, 'src/components'),
       '@services': path.resolve(__dirname, 'src/services'),
       '@utils': path.resolve(__dirname, 'src/utils'),
+      '@views': path.resolve(__dirname, 'src/views'),
     },
   },
   plugins: [react()],
