423fa0fe5deb83a717efcae1ffc9e288ba160993
Goncalo Adolfo <g.adolfo@runtime-revolution.com>
2023-04-26 15:28:07 +0100

    Set dark theme as default

--- movies-app/src/main.jsx
+++ movies-app/src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
 import App from './App'
 import './index.css'
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <ThemeProvider theme={darkTheme}>
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>
 )
