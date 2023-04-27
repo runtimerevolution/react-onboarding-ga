9976d2da20ff1412de9feb70610f47fc3a77c568
Goncalo Adolfo <g.adolfo@runtime-revolution.com>
2023-04-27 11:51:49 +0100

    Make home clickable

--- movies-app/src/components/NavigationBar.jsx
+++ movies-app/src/components/NavigationBar.jsx
@@ -2,6 +2,7 @@ import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
+import { Link } from 'react-router-dom'
 
 const NavigationBar = function () {
   return (
@@ -9,7 +10,7 @@ const NavigationBar = function () {
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Home
+            <Link to="/">Home</Link>
           </Typography>
         </Toolbar>
       </AppBar>
