72d369e89fb65caa0a8f8349a2df43f4bfdf5e24
Goncalo Adolfo <g.adolfo@runtime-revolution.com>
2023-04-27 11:32:34 +0100

    Fix merge issues

--- movies-app/vite.config.js
+++ movies-app/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import * as path from 'path'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
