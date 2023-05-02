c147e5786e578a67b9ddc108ccf0522970d310f5
Goncalo Adolfo <g.adolfo@runtime-revolution.com>
2023-05-02 17:14:38 +0100

    Refractor to use axios package

--- movies-app/src/services/TmdbApiClient.js
+++ movies-app/src/services/TmdbApiClient.js
@@ -1,55 +1,49 @@
+import axios from 'axios'
 import { Constants } from '@utils'
+
+const tmdbInstance = axios.create({
+  baseURL: Constants.TMDB_ENDPOINT,
+  params: { api_key: Constants.API_KEY },
+})
 
 const TmdbApiClient = {
   fetchTrendingMovies: function (timeWindow) {
-    return fetch(
-      `${Constants.TMDB_ENDPOINT}trending/movie/${timeWindow}?api_key=${Constants.API_KEY}`
-    )
+    return tmdbInstance
+      .get(`/trending/movie/${timeWindow}`)
       .then((res) => {
-        if (res.ok) {
-          return res.json()
+        if (res.status === 200) {
+          return res.data.results
         }
         throw res
       })
-      .then((data) => {
-        return data.results
-      })
       .catch((error) => {
-        console.log(`error fetching trending movies: ${error}`)
+        console.log(`error fetching trending movies: ${error.message}`)
       })
   },
   search: function (queryString) {
-    return fetch(
-      `${Constants.TMDB_ENDPOINT}search/movie?api_key=${Constants.API_KEY}&query=${queryString}`
-    )
+    return tmdbInstance
+      .get(`/search/movie`, { params: { query: queryString } })
       .then((res) => {
-        if (res.ok) {
-          return res.json()
+        if (res.status === 200) {
+          return res.data.results
         }
         throw res
       })
-      .then((data) => {
-        return data.results
-      })
       .catch((error) => {
-        console.log(`error searching: ${error}`)
+        console.log(`error searching: ${error.message}`)
       })
   },
   fetchMovieDetails: function (movieId) {
-    return fetch(
-      `${Constants.TMDB_ENDPOINT}movie/${movieId}?api_key=${Constants.API_KEY}`
-    )
+    return tmdbInstance
+      .get(`/movie/${movieId}`)
       .then((res) => {
-        if (res.ok) {
-          return res.json()
+        if (res.status === 200) {
+          return res.data
         }
         throw res
       })
-      .then((data) => {
-        return data
-      })
       .catch((error) => {
-        console.log(`error fetching movie details: ${error}`)
+        console.log(`error fetching movie details: ${error.message}`)
       })
   },
 }
