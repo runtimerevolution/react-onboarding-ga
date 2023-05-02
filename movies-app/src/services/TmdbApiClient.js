d07c8d2f6d64c70420278353b2d3557b7d5fcd47
Gonçalo Adolfo <56698352+goncaloadolfo@users.noreply.github.com>
2023-05-02 09:18:11 +0100

    [#7] Show the search results (#16)

--- movies-app/src/services/TmdbApiClient.js
+++ movies-app/src/services/TmdbApiClient.js
@@ -18,6 +18,23 @@ const TmdbApiClient = {
         console.log(`error fetching trending movies: ${error}`)
       })
   },
+  search: function (queryString) {
+    return fetch(
+      `${Constants.TMDB_ENDPOINT}search/multi?api_key=${Constants.API_KEY}&query=${queryString}`
+    )
+      .then((res) => {
+        if (res.ok) {
+          return res.json()
+        }
+        throw res
+      })
+      .then((data) => {
+        return data.results
+      })
+      .catch((error) => {
+        console.log(`error searching: ${error}`)
+      })
+  },
 }
 
 export default TmdbApiClient
--- movies-app/src/views/SearchResultsPage.jsx
+++ movies-app/src/views/SearchResultsPage.jsx
@@ -1,10 +1,60 @@
+import { useEffect, useRef, useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
+import { TmdbApiClient } from '@services'
+import { Constants } from '@utils'
 
 const SearchResultsPage = function () {
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
+  const nrImagesLoaded = useRef(0)
   const [searchParams] = useSearchParams()
   const queryParam = searchParams.get('query')
 
-  return <div>Search results for: {queryParam}</div>
+  const onCompleteImg = function () {
+    nrImagesLoaded.current++
+    if (nrImagesLoaded.current === data.length) {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    TmdbApiClient.search(queryParam)
+      .then((data) => {
+        setData(data)
+        if (data.length === 0) {
+          setLoading(false)
+        }
+      })
+      .catch((error) => {
+        setLoading(false)
+        console.log(error)
+      })
+  }, [])
+
+  return (
+    <>
+      {loading ? (
+        <span>loading...</span>
+      ) : (
+        data.length === 0 && <span>No results found.</span>
+      )}
+      <div
+        className="search-results-container"
+        style={{ display: loading ? 'none' : 'flex' }}
+      >
+        {data.map((item) => (
+          <img
+            key={item.id}
+            className="search-result-img"
+            src={`${Constants.IMAGE_API_ENDPOINT}${item.poster_path}`}
+            alt={`search result ${item.title || item.name}`}
+            onLoad={onCompleteImg}
+            onError={onCompleteImg}
+          />
+        ))}
+      </div>
+    </>
+  )
 }
 
 export default SearchResultsPage
