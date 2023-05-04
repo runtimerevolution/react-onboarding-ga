28271ca6ea34f6163d836b722350a560279b1efa
Goncalo Adolfo <g.adolfo@runtime-revolution.com>
2023-05-04 16:11:16 +0100

    Handle form submission

--- movies-app/src/views/LibraryPage.jsx
+++ movies-app/src/views/LibraryPage.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react'
-import { Form } from 'react-router-dom'
+import { useState } from 'react'
+import { useForm, Controller } from 'react-hook-form'
 import { Button, Stack, TextField, Typography } from '@mui/material'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker'
@@ -8,15 +8,19 @@ import { LibraryApiClient } from '@services'
 
 const LibraryPage = function () {
   const [res, setRes] = useState()
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      name: '',
+      biography: '',
+      nationality: '',
+      birth_date: '',
+    },
+  })
 
-  useEffect(() => {
-    // LibraryApiClient.createAuthor({
-    //   name: 'new author gonçalo',
-    //   biography: 'new author bio',
-    //   birth_date: '1999-02-21',
-    //   nationality: 'portuguese',
-    // }).then((res) => setRes(res))
-  }, [])
+  const onSubmit = function (data) {
+    data.birth_date = data.birth_date.format('YYYY-MM-DD')
+    LibraryApiClient.createAuthor(data).then((res) => setRes(res))
+  }
 
   return (
     <>
@@ -25,26 +29,57 @@ const LibraryPage = function () {
       ) : (
         <div>
           <Typography variant="h4">Create an Author</Typography>
-          <Form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <Stack spacing={5} sx={{ marginTop: '20px' }}>
-              <TextField id="name-text-field" label="Name" variant="outlined" />
-              <TextField
-                id="biography-text-field"
-                label="Biography"
-                variant="outlined"
-                multiline
-                rows={10}
+              <Controller
+                name="name"
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    id="name-text-field"
+                    label="Name"
+                    variant="outlined"
+                    {...field}
+                  />
+                )}
+              />
+              <Controller
+                name="biography"
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    id="biography-text-field"
+                    label="Biography"
+                    variant="outlined"
+                    multiline
+                    rows={10}
+                    {...field}
+                  />
+                )}
               />
               <Stack direction="row" spacing={5}>
-                <TextField
-                  id="nationality-text-field"
-                  className="width-300"
-                  label="Nationality"
-                  variant="outlined"
+                <Controller
+                  name="nationality"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      id="nationality-text-field"
+                      className="width-300"
+                      label="Nationality"
+                      variant="outlined"
+                      {...field}
+                    />
+                  )}
                 />
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <DatePicker label="Birth Date" />
-                </LocalizationProvider>
+                <Controller
+                  name="birth_date"
+                  control={control}
+                  render={({ field }) => (
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                      <DatePicker label="Birth Date" {...field} />
+                    </LocalizationProvider>
+                  )}
+                />
               </Stack>
             </Stack>
             <Button
@@ -55,7 +90,7 @@ const LibraryPage = function () {
             >
               Create
             </Button>
-          </Form>
+          </form>
         </div>
       )}
     </>
