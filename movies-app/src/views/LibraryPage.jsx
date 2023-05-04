c10a31fd55161c11f7a439fded0f9395036189f0
Goncalo Adolfo <g.adolfo@runtime-revolution.com>
2023-05-04 13:03:52 +0100

    Add form UI

--- movies-app/src/views/LibraryPage.jsx
+++ movies-app/src/views/LibraryPage.jsx
@@ -1,5 +1,38 @@
+import { Form } from 'react-router-dom'
+import { Stack, TextField, Typography } from '@mui/material'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+
 const LibraryPage = function () {
-  return <div>Library Form</div>
+  return (
+    <>
+      <Typography variant="h2">Create an Author</Typography>
+      <Form>
+        <Stack spacing={5} sx={{ marginTop: '10px' }}>
+          <TextField id="name-text-field" label="Name" variant="outlined" />
+          <TextField
+            id="biography-text-field"
+            label="Biography"
+            variant="outlined"
+            multiline
+            rows={10}
+          />
+          <Stack direction="row" spacing={5}>
+            <TextField
+              id="nationality-text-field"
+              className="width-300"
+              label="Nationality"
+              variant="outlined"
+            />
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker label="Birth Date" />
+            </LocalizationProvider>
+          </Stack>
+        </Stack>
+      </Form>
+    </>
+  )
 }
 
 export default LibraryPage
