60426e3596f88cdbbb06085d4ec3212e42d3d835
Goncalo Adolfo <g.adolfo@runtime-revolution.com>
2023-05-04 14:22:46 +0100

    Add library endpoint

--- movies-app/src/utils/Constants.js
+++ movies-app/src/utils/Constants.js
@@ -2,6 +2,7 @@ const Constants = {
   API_KEY: import.meta.env.VITE_TMDB_KEY,
   TMDB_ENDPOINT: import.meta.env.VITE_TMDB_ENDPOINT,
   IMAGE_API_ENDPOINT: import.meta.env.VITE_IMAGE_API_ENDPOINT,
+  LIBRARY_ENDPOINT: import.meta.env.VITE_LIBRARY_ENDPOINT,
 }
 
 export default Constants
