b90aec71014f971b5a736873207f102bc1cdadcf
Goncalo Adolfo <g.adolfo@runtime-revolution.com>
2023-05-04 15:09:37 +0100

    Add library api client

--- movies-app/src/views/LibraryPage.jsx
+++ movies-app/src/views/LibraryPage.jsx
@@ -1,36 +1,63 @@
+import { useEffect, useState } from 'react'
 import { Form } from 'react-router-dom'
-import { Stack, TextField, Typography } from '@mui/material'
+import { Button, Stack, TextField, Typography } from '@mui/material'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { LibraryApiClient } from '@services'
 
 const LibraryPage = function () {
+  const [res, setRes] = useState()
+
+  useEffect(() => {
+    // LibraryApiClient.createAuthor({
+    //   name: 'new author gonçalo',
+    //   biography: 'new author bio',
+    //   birth_date: '1999-02-21',
+    //   nationality: 'portuguese',
+    // }).then((res) => setRes(res))
+  }, [])
+
   return (
     <>
-      <Typography variant="h2">Create an Author</Typography>
-      <Form>
-        <Stack spacing={5} sx={{ marginTop: '10px' }}>
-          <TextField id="name-text-field" label="Name" variant="outlined" />
-          <TextField
-            id="biography-text-field"
-            label="Biography"
-            variant="outlined"
-            multiline
-            rows={10}
-          />
-          <Stack direction="row" spacing={5}>
-            <TextField
-              id="nationality-text-field"
-              className="width-300"
-              label="Nationality"
-              variant="outlined"
-            />
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DatePicker label="Birth Date" />
-            </LocalizationProvider>
-          </Stack>
-        </Stack>
-      </Form>
+      {res ? (
+        <pre>{JSON.stringify(res, null, 2)}</pre>
+      ) : (
+        <div>
+          <Typography variant="h4">Create an Author</Typography>
+          <Form>
+            <Stack spacing={5} sx={{ marginTop: '20px' }}>
+              <TextField id="name-text-field" label="Name" variant="outlined" />
+              <TextField
+                id="biography-text-field"
+                label="Biography"
+                variant="outlined"
+                multiline
+                rows={10}
+              />
+              <Stack direction="row" spacing={5}>
+                <TextField
+                  id="nationality-text-field"
+                  className="width-300"
+                  label="Nationality"
+                  variant="outlined"
+                />
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker label="Birth Date" />
+                </LocalizationProvider>
+              </Stack>
+            </Stack>
+            <Button
+              type="submit"
+              variant="contained"
+              size="large"
+              sx={{ marginTop: '20px' }}
+            >
+              Create
+            </Button>
+          </Form>
+        </div>
+      )}
     </>
   )
 }
